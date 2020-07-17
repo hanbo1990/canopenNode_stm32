722624841a25e58fe8e828c0b05d16f321d6140e
hanbo1990 <hanbo1990@gmail.com>
2020-07-17 20:02:11 +0200

    working usb com poc

--- app/applications/main.c
+++ app/applications/main.c
@@ -18,12 +18,74 @@
 #define DBG_LVL DBG_LOG
 #define APP_VERSION "v0.0.0"
 
+rt_device_t dev = RT_NULL;
+static struct rt_semaphore rx_sem;
+
+rt_size_t data_size;
+
+static rt_err_t usb_input(rt_device_t dev, rt_size_t size)
+{
+    rt_sem_release(&rx_sem);
+    rt_kprintf("rx:%d...\n", size);
+    data_size = size;
+    return RT_EOK;
+}
+
+static void serial_thread_entry(void *parameter)
+{
+    char* ch;
+
+    while (1)
+    {
+        rt_sem_take(&rx_sem, RT_WAITING_FOREVER);
+
+        ch = (char*) rt_calloc(1, data_size + 1);
+
+        if(NULL != ch){
+            rt_device_read(dev, -1, ch, data_size);
+            rt_kprintf("received:%s\n", ch);
+            rt_free(ch);
+            ch = NULL;
+        } else {
+            rt_kprintf("failed to malloc\n");
+        }
+    }
+}
+
 int main(void)
 {
     rt_kprintf("System start up, current version is %s\n", APP_VERSION);
 
     (void) hw_init();
     (void) apppool_init();
+
+    rt_thread_mdelay(2000);
+
+    char buf[] = "hello rt-thread!\r\n";
+
+    dev = rt_device_find("vcom");
+
+    rt_sem_init(&rx_sem, "rx_sem", 0, RT_IPC_FLAG_FIFO);
+
+    if (dev){
+        rt_kprintf("find vcom\n");
+        rt_device_open(dev, RT_DEVICE_FLAG_RDWR | RT_DEVICE_FLAG_INT_RX);
+    }
+    else{
+        rt_kprintf("cannot find vcom\n");
+        return -RT_ERROR;
+    }
+
+    rt_device_set_rx_indicate(dev, usb_input);
+
+    rt_thread_t thread = rt_thread_create("serial", serial_thread_entry, RT_NULL, 1024, 25, 10);
+    rt_thread_startup(thread);
+
+    while (1)
+    {
+        rt_device_write(dev, 0, buf, rt_strlen(buf));
+        rt_thread_mdelay(1000);
+    }
 
     return RT_EOK;
 }
--- app/rtconfig.h
+++ app/rtconfig.h
@@ -106,6 +106,16 @@
 
 /* Using USB */
 
+#define RT_USING_USB_DEVICE
+#define RT_USBD_THREAD_STACK_SZ 4096
+#define USB_VENDOR_ID 0x0FFE
+#define USB_PRODUCT_ID 0x0001
+#define _RT_USB_DEVICE_CDC
+#define RT_USB_DEVICE_CDC
+#define RT_VCOM_TASK_STK_SIZE 512
+#define RT_VCOM_SERNO "32021919830108"
+#define RT_VCOM_SER_LEN 14
+#define RT_VCOM_TX_TIMEOUT 1000
 /* end of Using USB */
 /* end of Device Drivers */
 
